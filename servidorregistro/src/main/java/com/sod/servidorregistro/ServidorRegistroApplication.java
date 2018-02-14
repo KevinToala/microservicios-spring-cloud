fb4ec54339914179fd787994d67c2014615c1006
CarrionP <kevin.carrionp@ug.edu.ec>
2018-02-13 23:30:14 -0500

    Mejoras

--- servidorregistro/src/main/java/com/sod/servidorregistro/ServidorRegistroApplication.java
+++ servidorregistro/src/main/java/com/sod/servidorregistro/ServidorRegistroApplication.java
@@ -2,10 +2,13 @@ package com.sod.servidorregistro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EnableEurekaServer
 @SpringBootApplication
+@EntityScan(basePackages = "com.sod.servidorregistro",basePackageClasses = Jsr310JpaConverters.class)
 public class ServidorRegistroApplication {
 
 	public static void main(String[] args) {
