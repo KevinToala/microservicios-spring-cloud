cdbd8882b55cb3eb295c4280fc1295ae7142cd05
Kevin Toala <kevin.toala@outlook.com>
2018-03-15 02:47:11 -0500

    Revert "Mejoras"
    This reverts commit fb4ec54

--- clientefacturacion/src/main/java/com/sod/clientefacturacion/ClientefacturacionApplication.java
+++ clientefacturacion/src/main/java/com/sod/clientefacturacion/ClientefacturacionApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
 public class ClientefacturacionApplication {
--- servidorregistro/src/main/java/com/sod/servidorregistro/ServidorRegistroApplication.java
+++ servidorregistro/src/main/java/com/sod/servidorregistro/ServidorRegistroApplication.java
@@ -2,13 +2,10 @@ package com.sod.servidorregistro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EnableEurekaServer
 @SpringBootApplication
-@EntityScan(basePackages = "com.sod.servidorregistro",basePackageClasses = Jsr310JpaConverters.class)
 public class ServidorRegistroApplication {
 
 	public static void main(String[] args) {
