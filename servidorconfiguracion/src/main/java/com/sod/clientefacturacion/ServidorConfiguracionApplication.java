6eaff8252fd41edbc0c1cb5c911239b4c4dde62a
Kevin Toala <kevin.toala@outlook.com>
2018-03-15 02:15:45 -0500

    Config servidor registro

--- servidorconfiguracion/src/main/java/com/sod/clientefacturacion/ServidorConfiguracionApplication.java
+++ servidorconfiguracion/src/main/java/com/sod/clientefacturacion/ServidorConfiguracionApplication.java
@@ -2,10 +2,7 @@ package com.sod.clientefacturacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -13,11 +10,5 @@ public class ServidorConfiguracionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServidorConfiguracionApplication.class, args);
-	}
-	
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate(){
-		return new RestTemplate();
 	}
 }
